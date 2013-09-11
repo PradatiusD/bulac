c3ff41ae43ea07ee85c252baa7ca663c4c279d33
PradatiusD <danielprada2012@gmail.com>
2013-09-11 12:07:12 -0400

    Added meta tag support for header, made the default for pages to not show comments,  and made a page template that supported comments.

--- BULAC/header.php
+++ BULAC/header.php
@@ -21,6 +21,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
--- BULAC/page-with-comments.php
+++ BULAC/page-with-comments.php
@@ -1,15 +1,17 @@
 <?php
+/*
+Template Name: Page with Comments
+*/
+
 /**
- * The template for displaying all pages.
+ * The template for displaying a page with comments below.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that other
- * 'pages' on your WordPress site will use a different template.
  *
  * @package WordPress
  * @subpackage Twenty_Thirteen
  * @since Twenty Thirteen 1.0
  */
+
 
 get_header(); ?>
 
@@ -40,6 +42,7 @@ get_header(); ?>
 					</footer><!-- .entry-meta -->
 				</article><!-- #post -->
 
+				<?php comments_template(); ?>
 			<?php endwhile; ?>
 
 		</div><!-- #content -->
