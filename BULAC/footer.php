f4e13dad101f07e9e8be38f6706008ddd0eabed1
PradatiusD <danielprada2012@gmail.com>
2014-03-11 18:56:23 -0400

    Added footer logo

--- BULAC/footer.php
+++ BULAC/footer.php
@@ -1,4 +1,11 @@
 		<?php wp_footer();?>
+		<footer class="row">
+			<div class="large-12 columns text-center">
+				<hr>
+				<img src="<?php echo get_template_directory_uri().'/bulac-black-white-small.png';?>" alt="">
+				<br><br>			
+			</div>
+		</footer>
 		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/bulac.min.js"></script>
 	</body>
 </html>
